29b7a210d13bfb28ea146d085d42dac446ef82dd
曹文成 <845982120@qq.com>
2017-09-23 16:15:14 +0800

    redux 日志系统

--- index.jsx
+++ index.jsx
@@ -5,6 +5,8 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider, connect } from 'react-redux'
 // 引入reducer
 import reducer from 'state/reducers'
+// 引入redux日志系统中间件
+import { createLogger } from 'redux-logger'
 import Routes from '@/routes/Routes'
 import global from 'static/scss/global.scss'
 
@@ -13,8 +15,8 @@ var initState = {
   count: 0
 }
 
-// 创建store
-const store = createStore(reducer, initState)
+// 创建store 初始化状态 应用redux日志系统中间件
+const store = createStore(reducer, initState, applyMiddleware(createLogger()))
 
 ReactDOM.render(
     <Provider store={store}>
