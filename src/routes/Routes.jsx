70bdde13ae0a490191320f3d6e91ba150f65e1af
曹文成 <845982120@qq.com>
2017-09-23 15:01:51 +0800

    路由匹配

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 //Components
 import Homepage from 'pages/Homepage'
 import NotFound from 'pages/NotFound'
 
+// 如果你的Home，Topics， Jobs是互斥的，那还要加上Switch
+// 这里指定exact意思是精确匹配，为了给其他组件一个机会获取到url改变
 class Routes extends Component {
 	render() {
 		return (
 			<Router>
-				<Switch>
-                	<Route exact path="/" component={ Homepage } />
-					<Route path="/home" component={ Homepage } />
-					<Route component={ NotFound } />
-				</Switch>
+				<div>
+					<Switch>
+						<Route exact path="/" component={ Homepage } />
+						<Route exact path={`/home`} component={ Homepage } />
+						<Route component={ NotFound } />
+					</Switch>
+				</div>
             </Router>
 		);
 	}
